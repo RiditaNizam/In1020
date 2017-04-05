80939ee11444e37475fd020c0564f3e2923cfcb4
RiditaNizam <riditan@gmail.com>
2017-04-04 22:23:42 -0400

    Initial commit

---
+++ src/com/company/In1020.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Created by ridita on 4/4/17.
+ */
+public class In1020 {
+
+    public boolean in1020(int a, int b) {
+
+        if ((a >= 10 && a <= 20) || (b >= 10 && b <= 20)) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    In1020 in1020Object = new In1020();
+    }
+}
